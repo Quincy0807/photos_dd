65b878368079ebecb05b86aaecacb47774fb7881
Quincy <q.c.yamblbq@gmail.com>
2016-06-29 17:01:48 +0800

    add upload button

--- build/js/index.js
+++ build/js/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 
 require('../../public/css/uikit.min.css')
 require('../sass/index.scss')
+
 new Vue({
   el: "#buttons",
   data: {
     allPhotos: "Check Daily Breakfast",
-    withMap: "Check Locations"
+    withMap: "Check Locations",
+    upload: "Upload"
   }
 })
